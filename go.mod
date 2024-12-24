e922f6b23e88615b19d0aedbd9fb1489ecefe538
pasu <pascal.stockert@live.com>
2024-12-24 01:49:35 +0100

    add sample cron

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-webhook
 
 go 1.23
+
+require github.com/robfig/cron v1.2.0
--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/robfig/cron"
 	"log"
 	"net/http"
 )
+
+type CronEntry struct {
+	name    string
+	spec    string
+	closure func()
+}
 
 func jsonMiddleware(closure http.HandlerFunc) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
@@ -23,6 +30,25 @@ func healthHandler(res http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	cronEntries := []CronEntry{
+		{
+			name:    "test",
+			spec:    "@every 5s",
+			closure: func() { log.Println("test cron") },
+		},
+	}
+
+	c := cron.New()
+
+	for _, entry := range cronEntries {
+		e := c.AddFunc(entry.spec, entry.closure)
+		if e != nil {
+			log.Fatalf("error adding cron entry: %v", e)
+		}
+	}
+
+	c.Start()
+
 	http.HandleFunc("/health", jsonMiddleware(healthHandler))
 
 	log.Println(http.ListenAndServe(":4321", nil))
