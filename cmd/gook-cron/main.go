0091d7a1eb5f9d0e6baf1492be39a644d77e0ef8
Pascal Stockert <pascal.stockert@live.com>
2025-03-05 23:02:45 +0100

    add draft of sending requests

--- cmd/gook-cron/main.go
+++ cmd/gook-cron/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/robfig/cron"
 	"go-webhook/pkg/env"
 	"go-webhook/pkg/files"
+	"go-webhook/pkg/types"
 )
 
 func main() {
@@ -32,6 +34,22 @@ func main() {
 	// TODO store relation between cronjob and csv-entry to keep track of stopped/started jobs
 	for _, entry := range entries {
 		_ = c.AddFunc(entry.Spec, func() {
+			if entry.Action.Type == types.CronActionTypeHttp {
+				req, err := http.NewRequest("GET", "https://"+entry.Action.Resource, nil)
+				if err != nil {
+					fmt.Println("could not create request", err)
+					return
+				}
+
+				res, err := http.DefaultClient.Do(req)
+				if err != nil {
+					fmt.Println("could not send request", err)
+					return
+				}
+
+				fmt.Println(res.StatusCode)
+			}
+
 			// TODO determine action to execute
 			fmt.Println(entry.Action)
 		})
